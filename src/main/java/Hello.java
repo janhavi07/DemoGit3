d8893521f16f018ab5eeaf0965e7c05c3e2880c5
Janhavi Parte <janhavi.parte@Janhavis-MacBook-Pro.local>
2020-02-25 14:40:36 +0530

    A int variable added

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         String text1="Byeee";
+        int no1=887;
         System.out.printf("Hey world");
         System.out.printf("You are beautiful");
     }
