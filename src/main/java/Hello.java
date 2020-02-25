b8d19e7c533695952a4dfabedff590ab5ae7ebf0
Janhavi Parte <janhavi.parte@Janhavis-MacBook-Pro.local>
2020-02-25 14:46:47 +0530

    Added tw

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         String text1="Byeee";
         int no1=887;
+        String tw="Crazy minds";
         System.out.printf("Hey world");
         System.out.printf("You are beautiful");
     }
